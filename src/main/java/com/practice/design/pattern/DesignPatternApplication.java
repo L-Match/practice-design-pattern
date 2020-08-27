44357e4866bdf542d4501e423e700f8b8dc1277f
liuchang <15735926905@163.com>
2020-08-27 16:18:16 +0800

    rename starter

--- src/main/java/com/practice/design/pattern/DesignPatternApplication.java
+++ src/main/java/com/practice/design/pattern/DesignPatternApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatternApplication {
+public class DesignPatternApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PatternApplication.class, args);
+        SpringApplication.run(DesignPatternApplication.class, args);
     }
 
 }
